50defe696c80b5bfaa554d0483831438633906e6
sunilbelde <47601858+sunilbelde@users.noreply.github.com>
2021-02-13 10:58:06 +0530

    Update s3.py

--- s3.py
+++ s3.py
@@ -1,10 +1,14 @@
 import boto3
 import os 
 
+
+#Connecting aws S3 
+#Note: You need to pass AWS ACCESS KEY and PASSWORD from aws cli or mention in your enironment variables
+
 client = boto3.client('s3', region_name='ap-south-1')
 
-        
+BUCKET_NAME='YOUR-BUCKET-NAME'       
 for file in os.listdir("static"):
   if file.endswith('.jpg'):
     print(file)
-    client.upload_file('static/'+file, 'flaskappfashion', file, ExtraArgs={'ContentType': "image/jpeg", 'ACL': "public-read"})
+    client.upload_file('static/'+file, BUCKET_NAME , file, ExtraArgs={'ContentType': "image/jpeg", 'ACL': "public-read"}) # Uploading files to BUCKET
